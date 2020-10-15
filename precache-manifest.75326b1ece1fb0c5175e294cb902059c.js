c5c5dd0275902a19b5296a8d951c8cea35de39ff
komugiko111 <satoworkspace@gmail.com>
2020-10-15 17:17:03 +0900

    Updates

---
+++ precache-manifest.75326b1ece1fb0c5175e294cb902059c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f102c041bab05d47c035f03caf51ab1",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "90f92fae338e43eb4647",
+    "url": "/simple-todo/static/css/main.5da3d4d7.chunk.css"
+  },
+  {
+    "revision": "7aa80c0fd946e239b8aa",
+    "url": "/simple-todo/static/js/2.1e357300.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo/static/js/2.1e357300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f92fae338e43eb4647",
+    "url": "/simple-todo/static/js/main.68720493.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
